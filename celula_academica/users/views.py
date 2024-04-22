1517ed31126a46aa5a038490416767c4efab3618
badilleins <badilleins@gmail.com>
2024-04-21 19:40:14 -0500

    Se ha acabado el inicio de sesión con manejo de token de usuario y envio de correo electronico

--- celula_academica/users/views.py
+++ celula_academica/users/views.py
@@ -38,3 +38,28 @@ class Register(APIView):
 
 
 
+class Login(APIView):
+    def post(self, request):
+        try:
+     
+            email = request.data.get('email')
+            password = request.data.get('password')
+
+            if not email or not password:
+                return Response({'error': 'Debes proporcionar un correo electrónico y una contraseña'}, status=400)
+
+            supabase_url = 'https://esdqjitrbfjfnpkmizyx.supabase.co'
+            supabase_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzZHFqaXRyYmZqZm5wa21penl4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMyMjEwODYsImV4cCI6MjAyODc5NzA4Nn0.LxIA5PdtDASqPQHCWPFz8zTzVO83-oEPyTUJp_qUaIs'
+            client = create_client(supabase_url, supabase_key)
+
+  
+            auth_response = client.auth.sign_in(email, password)
+          
+        
+            if 'status_code' in auth_response and auth_response['status_code'] == 200:
+                # Autenticación exitosa
+                  return Response({'token': auth_response['access_token']})
+            else:
+                return Response({'error': auth_response.get('error', auth_response)}, status=400)
+        except Exception as e:
+            return Response({'error': auth_response}, status=500)
