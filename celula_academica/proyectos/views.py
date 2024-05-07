2916323eb95468243fc791c16672420fa68262a5
Edison <solisedison@outlook.com>
2024-05-06 22:14:09 -0500

    created the model pryects and configurated the admin

--- celula_academica/proyectos/views.py
+++ celula_academica/proyectos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework import status 
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import permission_classes , authentication_classes
+from rest_framework.authentication import TokenAuthentication
+@api_view(['GET'])
+def hello(request):
+    return Response({'message':'Hello World!'})
