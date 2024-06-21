5041e769173c5c71893e4502227e967f4b9700fa
Edison <solisedison@outlook.com>
2024-06-21 12:00:40 -0500

    fixed the superuser

--- build.sh
+++ build.sh
@@ -13,17 +13,19 @@ python manage.py migrate
 # Crea un superusuario (sustituye 'superuser' por los valores que desees)
 echo "from users.models import CustomUser
 from django.contrib.auth.models import Group
-group = Group.objects.create(name='Administradores')
-superuser = CustomUser.objects.create_superuser(
-    email='eortiz5364@uta.edu.ec',
-    cedula='1850085364',
-    telefono='0983860122',
-    carrera='Software',
-    semestre=6,
-    categoria=group,
-    password='123456',
-    nombre='Edison',
-    apellido='Ortiz'
+
+group, created = Group.objects.get_or_create(name='Administradores')
+if not CustomUser.objects.filter(email='eortiz5364@uta.edu.ec').exists():
+    CustomUser.objects.create_superuser(
+        email='eortiz5364@uta.edu.ec',
+        cedula='1850085364',
+        telefono='0983860122',
+        carrera='Software',
+        semestre=6,
+        categoria=group,
+        password='123456',
+        nombre='Edison',
+        apellido='Ortiz'
 )" | python manage.py shell
 
 # Convert static asset files
