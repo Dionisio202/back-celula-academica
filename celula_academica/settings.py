1689a815ac5278202579d3aa25c9b1ebefb8d36a
Edison <solisedison@outlook.com>
2024-06-21 22:21:50 -0500

    finished s3 configuration

--- celula_academica/settings.py
+++ celula_academica/settings.py
@@ -154,3 +154,13 @@ DOMAIN_NAME = 'https://admin-celula-academica.onrender.com'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.CustomUser'
 
+# AWS settings
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_SIGNATURE_NAME = 's3v4',
+AWS_S3_REGION_NAME =  os.environ.get('AWS_S3_REGION_NAME ')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL =  None
+AWS_S3_VERIFY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
